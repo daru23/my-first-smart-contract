1c4465e6f0afab65f58dbf45f91069b182dfff4c
Daniela Ruiz <daru015@gmail.com>
2021-09-04 09:43:11 +0200

    Add: compile.js script to compile Solidity contracts

---
+++ compile.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+// Compile contract
+const contractPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+};
+let contractFile;
+
+try {
+    const tempFile = JSON.parse(solc.compile(JSON.stringify(input)), 1);
+    contractFile = tempFile.contracts['Inbox.sol']['Inbox'];
+} catch (e) {
+    contractFile = {};
+}
+
+module.exports = contractFile;
