dd8699f787a6e0d5bd501dbd0cca1cef0f2226a6
Daniela Ruiz <daru015@gmail.com>
2021-09-04 10:05:38 +0200

    Feat: compile script accept argument to dynamically compile contracts

--- compile.js
+++ compile.js
@@ -2,6 +2,12 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
+// Getting arguments
+let contractName = 'Inbox.sol'
+if (process.argv[2]) {
+    contractName = process.argv[2];
+}
+
 // Compile contract
 const contractPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(contractPath, 'utf8');
